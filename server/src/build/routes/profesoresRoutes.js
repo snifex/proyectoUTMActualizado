169c51abe706e0d5be07caa85a68c05a6a6adcc2
snifex <58663377+snifex@users.noreply.github.com>
2022-03-23 18:42:18 -0600

    Clase 23/03/2022

--- server/src/build/routes/profesoresRoutes.js
+++ server/src/build/routes/profesoresRoutes.js
@@ -2,13 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const profesoresController_1 = require("../controllers/profesoresController");
+const auth_1 = require("../middleware/auth");
 class ProfesoresRoutes {
     constructor() {
         this.router = (0, express_1.Router)();
         this.config();
     }
     config() {
-        this.router.get('/', profesoresController_1.profesoresController.list);
+        this.router.get('/', auth_1.validarToken, profesoresController_1.profesoresController.list);
         this.router.get('/:idProfesor', profesoresController_1.profesoresController.listOne);
         this.router.post('/create', profesoresController_1.profesoresController.create);
         this.router.put('/actualizar/:idProfesor', profesoresController_1.profesoresController.actualizar);
---
+++ src/app/models/header.model.ts
@@ -0,0 +1,2 @@
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+export const Headers = new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.bm9ibHNlaXM4OEBnbWFpbC5jb20.5-oCs8iGorHyTkIx1bGTeUDEwPehASLVDlaLtaHOwBs');
